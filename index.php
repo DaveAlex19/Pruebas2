9fa7d338315a3d423a01c37dd5d82d9f4fa24dfc
EdwinMLara <emlara35@gmail.com>
2019-11-26 21:18:29 -0600

    agregando un contenerdor y tres div

--- index.php
+++ index.php
@@ -7,5 +7,12 @@
 <body>
 	<p>Que pedo pinche davis</p>
 	<p>segundo parrafo</p>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">Div 1</div>
+			<div class="col-md-6">Div 2</div>
+			<div class="col-md-6">Div 3</div>
+		</div>
+	</div>
 </body>
 </html>
